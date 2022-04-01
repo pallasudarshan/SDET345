65451d360c7e18c30debfa6a052e64a2fcc06c97
pallasuda <pallasudarshan15@gmail.com>
2022-04-01 20:41:25 +0530

    invoiceTextadded

---
+++ src/test/java/com/testyanra/CreateInvoiceTest.java
@@ -0,0 +1,11 @@
+package com.testyanra;
+
+import org.testng.annotations.Test;
+
+public class CreateInvoiceTest {
+	@Test
+	public void CreateInvoiceText() {
+		System.out.println("added");
+	}
+
+}
