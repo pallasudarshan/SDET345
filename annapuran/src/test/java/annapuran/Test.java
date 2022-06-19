709681cf857edebafa25439b08e28215e61f15dd
pallasuda <pallasudarshan15@gmail.com>
2022-06-19 10:58:24 +0530

    purna

---
+++ annapuran/src/test/java/annapuran/Test.java
@@ -0,0 +1,30 @@
+package annapuran;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.selenium.dev/");
+		 List<WebElement> drivere = driver.findElements(By.xpath("//li[@class='nav-item mr-4 mb-2 mb-lg-0']/following::a[@class='nav-link']"));
+		 for(WebElement wb:drivere)
+		 {
+			 String text=wb.getText();
+			 System.out.println(text);
+			
+			
+		 }
+
+}
---
+++ annapuran/src/test/java/annapuran/Ts.java
@@ -0,0 +1,23 @@
+package annapuran;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class Ts {
+
+	public static void main(String[] args) throws SQLException {
+		//Register the driver
+		Driver driverRef = new Driver();
+		DriverManager.registerDriver(driverRef);
+		
+		// Create the connection from database;
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/projects","root","root");
+		
+		//create Sql statement
+		
+		con.createSQLXML();
+
+}
